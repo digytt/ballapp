3c744986dbfcf69bdd0331724ea5902160833f07
digytt <digytt063@gmail.com>
2025-12-10 13:53:36 -0700

    Initial Commit

---
+++ ballapp/ballappApp.swift
@@ -0,0 +1,17 @@
+//
+//  ballappApp.swift
+//  ballapp
+//
+//  Created by Digytt on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ballappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
